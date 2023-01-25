fa260ee01168fcb216b13678b4f25e50d1e24729
mdmohsin2 <mohsinbd862554@gmail.com>
2023-01-26 01:12:20 +0600

    design improve

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './Routes/Routes';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    AOS.init()
+  },[])
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
