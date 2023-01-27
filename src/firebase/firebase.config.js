5c71ccf1a6ff63359b3950c32674b4573fcc225f
MRPrio <maksudurprio.mp@gmail.com>
2023-01-27 20:09:35 +0600

    firebase added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -18,18 +18,38 @@
 // export default app;
 
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBjsjZi5508xjSRhqt8vuFYQWoRXCEz4rk",
+//   authDomain:  "jobstack-endgame.firebaseapp.com",
+//   projectId:  "jobstack-endgame",
+//   storageBucket: "jobstack-endgame.appspot.com",
+//   messagingSenderId:  "980820076383",
+//   appId:  "1:980820076383:web:99c57aa0f483d328a1f8ea"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export default app;
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjsjZi5508xjSRhqt8vuFYQWoRXCEz4rk",
-  authDomain:  "jobstack-endgame.firebaseapp.com",
-  projectId:  "jobstack-endgame",
-  storageBucket: "jobstack-endgame.appspot.com",
-  messagingSenderId:  "980820076383",
-  appId:  "1:980820076383:web:99c57aa0f483d328a1f8ea"
+  apiKey: "AIzaSyCIQe1EOLQyahII3XvxWysqDXo69rIG8C4",
+  authDomain: "endgame-jobstack.firebaseapp.com",
+  projectId: "endgame-jobstack",
+  storageBucket: "endgame-jobstack.appspot.com",
+  messagingSenderId: "80645197743",
+  appId: "1:80645197743:web:14e4ab5f5c4d7c56a0295a"
 };
 
 // Initialize Firebase
