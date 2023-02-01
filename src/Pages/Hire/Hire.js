ae704a73f7152abf21bc965d1b173dfaacb3a432
SADIA AFSANA <afsanasadia2022@gmail.com>
2023-02-01 13:07:33 +0600

    color

--- src/Pages/Hire/Hire.js
+++ src/Pages/Hire/Hire.js
@@ -50,21 +50,21 @@ const Hire = () => {
     }
 
     return (
-        <div className="px-4 grid grid-cols-1 md:grid-cols-8 gap-4">
-            <div className="col-span-2 mt-5">
+        <div className="px-4 grid grid-cols-1 md:grid-cols-8 gap-4  ">
+            <div className="col-span-2 mt-5 ">
                 <div className="">
                     <div className='flex gap-4 ml-2 mt-2'>
                         <img className='h-10 w-10 rounded-full' src={singleUser?.profileImage} alt="" />
                         <span className='flex gap-2 mt-2'> {singleUser?.name}</span>
                     </div>
                     <div className='flex flex-col gap-2 my-5'>
-                        <Link to={`/jobs/${user?.email}`} className='btn btn-outline btn-primary'>My Posts</Link>
-                        <Link to='/addajob' className='btn btn-outline btn-primary'>Add a job</Link>
+                        <Link to={`/jobs/${user?.email}`} className='btn btn-outline btn-primary shadow-md'>My Posts</Link>
+                        <Link to='/addajob' className='btn btn-outline btn-primary shadow-md'>Add a job</Link>
 
                     </div>
                 </div>
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 mt-4 py-2 rounded-2xl drop-shadow-xl bg-slate-100 ">
                 {
                     data?.map(candidate => <Candidate
                         key={candidate._id}
--- src/Pages/Network/MyConnections/MyConnections.js
+++ src/Pages/Network/MyConnections/MyConnections.js
@@ -4,17 +4,17 @@ import MyConnectionsCard from '../MyConnectionsCard/MyConnectionsCard';
 
 const MyConnections = () => {
     return (
-        <div className="newsFeed px-4 grid grid-cols-1 md:grid-cols-8 gap-2">
-            <div className="hidden shadow-xl md:block col-span-6">
+        <div className="newsFeed px-4 grid grid-cols-1 md:grid-cols-8 gap-2 bg-slate-100 ">
+            <div className="hidden shadow-2xl drop-shadow-md md:block col-span-6 py-6">
                 <div className='m-5'>
-                    <p className='text-2xl font-bold'>You have 49 connections</p>
+                    <p className='text-2xl text-center font-bold text-slate-600 mb-3'>You have 49 connections</p>
                     <div className=''>
-                        <p className='flex justify-between'><span className='mt-2'>Filter:</span> <select className="select w-1/2 max-w-xs">
-                            <option>Recently Added</option>
-                            <option>Last Name</option>
-                            <option>First Name</option>
+                        <p className='flex justify-between'><span className='mt-3 font-bold text-lg text-red-500 '>Filter:</span> <select className="border-cyan-500 rounded-xl border-2 select w-1/2 max-w-xs">
+                            <option className='bg-red-200'>Recently Added</option>
+                            <option className='bg-cyan-200'>Last Name</option>
+                            <option className='bg-lime-200 '>First Name</option>
                         </select>
-                        <input type="text" placeholder="search" className="input input-bordered w-1/2 max-w-s" />
+                            <input type="text" placeholder="search" className="input border-cyan-500 border-2 input-bordered w-1/2 max-w-s rounded-xl" />
                         </p>
                         <div className="divider"></div>
                     </div>
