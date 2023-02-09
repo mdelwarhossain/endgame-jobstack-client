bd178ab0ff55cd81670cd3668b5c2cf6a5ce13b0
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2023-02-09 23:38:18 +0600

    frontend correction

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,43 +1,46 @@
-import React from 'react';
-import {
-  TypeAnimation
-} from 'react-type-animation';
-import banner from '../../../assest/images/header_bg.png'
-import { useContext } from 'react';
-import { InfoContext } from '../../../contexts/UserInfoProvider';
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import banner from "../../../assest/images/Devices-pana.png";
+import { useContext } from "react";
+import { InfoContext } from "../../../contexts/UserInfoProvider";
 
 const Banner = () => {
-
-  const { userDetails } = useContext(InfoContext)
-  console.log(userDetails)
+  const { userDetails } = useContext(InfoContext);
+  console.log(userDetails);
 
   return (
     <div data-theme="night">
       {/* style={{ backgroundImage: `url(${banner})`, backgroundSize: "cover", backgroundPosition: 'center center' }} */}
       <div className="hero py-28 hero-overlay bg-opacity-20">
         <div className="hero-content flex-col lg:flex-row">
-          <img data-aos="fade-right" data-aos-duration='2000' alt="" className="h-[480px] w-[400px] mt-10 rounded-lg shadow-2xl lg:mr-12 ring-2 ring-offset-4 dark:bg-gray-500 ring-violet-400 ring-offset-gray-800" src='https://i.ibb.co/VTL7FbY/p.jpg' />
+          <img
+            data-aos="fade-right"
+            data-aos-duration="2000"
+            alt=""
+            className="h-[480px] w-[500px] mt-10 rounded-lg shadow-2xl lg:mr-12 ring-2 ring-offset-4  0"
+            src={banner}
+          />
           <div>
-            <h2 className='text-5xl mt-14 text-white'>Wellcome To Jobstack Platform</h2>
-            <div className='text-orange-400 mt-4'>
+            <h2 className="text-5xl mt-14 text-white">
+              Empower Your Career with Jobstack
+            </h2>
+            <div className="text-orange-400 mt-4">
               <TypeAnimation
                 // Same String at the start will only be typed once, initially
-                sequence={[
-                  'A WEB DEVELOPER',
-                  1000,
-                  'A MERN STACK DEVELOPER',
-                  1000,
-                  'A FRONT END  DEVELOPER',
-                  1000,
-                ]}
+                sequence={["Connect", 1000, "Grow", 1000, "Succeed", 1000]}
                 speed={50} // Custom Speed from 1-99 - Default Speed: 40
-                style={{ fontSize: '2em' }}
+                style={{ fontSize: "2em" }}
                 wrapper="span" // Animation will be rendered as a <span>
                 repeat={Infinity} // Repeat this Animation Sequence infinitely
               />
             </div>
-            <div className='md:w-[550px] mt-6 text-white'>
-              <p>I am a web developer with an amazing ability to develop websites that are both functional and aesthetically pleasing. I have a strong under-standing of web standards and best practices.</p>
+            <div className="md:w-[650px] mt-6 text-white font-medium">
+              <p>
+                Jobstack is your ultimate career platform, connecting you with
+                job opportunities, professional networks, and the tools you need
+                to grow and succeed in your career. Join our community today and
+                take control of your future.
+              </p>
             </div>
           </div>
         </div>
--- src/Pages/NewsFeed/LeftSideCard/LeftSideCard.js
+++ src/Pages/NewsFeed/LeftSideCard/LeftSideCard.js
@@ -1,75 +1,96 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthProvider';
-import pp from '../../../assest/images/pp.jpg'
-import cp from '../../../assest/images/cp.jpg'
-import Loading from '../../../Shared/LoadingPage/LoadingPage';
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import pp from "../../../assest/images/pp.jpg";
+import cp from "../../../assest/images/cp.jpg";
+import Loading from "../../../Shared/LoadingPage/LoadingPage";
 
 const LeftSideCard = () => {
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
 
-  const { user } = useContext(AuthContext)
-  const [loading, setLoading] = useState(false)
-
-  const [currentUserDetails, setCurrentUserDetails] = useState()
+  const [currentUserDetails, setCurrentUserDetails] = useState();
   const waitTime = 1000;
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     const id = setInterval(() => {
-
       fetch(`http://localhost:5000/user/${user?.email}`)
-        .then(res => res.json())
-        .then(data => {
-          setCurrentUserDetails(data)
-          setLoading(false)
-        })
+        .then((res) => res.json())
+        .then((data) => {
+          setCurrentUserDetails(data);
+          setLoading(false);
+        });
     }, waitTime);
     return () => clearInterval(id);
   }, [user?.email]);
   return (
-    <div className='my-5'>
-      {
-        loading ? <Loading></Loading> :
-
-          <div className="max-w-xs drop-shadow-lg rounded-md shadow-md bg-lime-50  relative">
-            {
-              currentUserDetails?.bannerImage ? <img src={currentUserDetails.bannerImage} /> :
-                <img src={cp} alt="" className="object-cover object-center w-full rounded-t-md h-20 bg-gray-500" />
-            }
-            <div className="flex flex-col justify-between p-6 space-y-8">
-              {
-                currentUserDetails?.profileImage ? <img alt="Profile" className="absolute top-11 left-2/4 -ml-8  w-16 h-16 rounded-full ring-2 ring-offset-4 bg-gray-500 ring-violet-400 ring-offset-gray-800" src={currentUserDetails.profileImage} /> :
-
-                  <img alt="Profile" className="absolute top-11 left-2/4 -ml-8  w-16 h-16 rounded-full ring-2 ring-offset-4 bg-gray-500 ring-violet-400 ring-offset-gray-800" src={pp} />
-              }
-              <div className="space-y-2 text-center">
-                <Link to='/userProfile'><h2 className="text-sky-600 text-2xl font-semibold tracking-wide">{currentUserDetails?.name}</h2></Link>
-                <div>
-                  {
-                    currentUserDetails?.headline ? <p className="text-yellow-700 text-md">{currentUserDetails?.headline}</p> :
-
-                      <p className="text-red-700 text-md">Add A Headline</p>
-                  }
-                </div>
-              </div>
-              <hr />
-              <div className='hover:bg-gray-200 rounded-md'>
-                <Link to='#'>
-                  <div>
-                    <div className='flex items-center justify-between text-sm'>
-                      <h3 className='text-cyan-900'>Connection</h3>
-                      <p className='text-blue-400 font-bold'>21</p>
-                    </div>
-                    <h2 className='text-md font-bold text-cyan-900'>Grow Your Network</h2>
-                  </div>
-                </Link>
-              </div>
+    <div className="my-5">
+      {loading ? (
+        <Loading></Loading>
+      ) : (
+        <div className="max-w-xs drop-shadow-lg rounded-md shadow-md bg-lime-50  relative">
+          {currentUserDetails?.bannerImage ? (
+            <img src={currentUserDetails.bannerImage} className=" h-48" />
+          ) : (
+            <img
+              src={cp}
+              alt=""
+              className="object-cover object-center w-full rounded-t-md h-48 bg-gray-500"
+            />
+          )}
+          <div className="flex flex-col justify-between p-6 space-y-8">
+            {currentUserDetails?.profileImage ? (
+              <img
+                alt="Profile"
+                className="absolute top-11 left-2/4 -ml-8  w-16 h-16 rounded-full ring-2 ring-offset-4 bg-gray-500 ring-violet-400 ring-offset-gray-800"
+                src={currentUserDetails.profileImage}
+              />
+            ) : (
+              <img
+                alt="Profile"
+                className="absolute top-11 left-2/4 -ml-8  w-16 h-16 rounded-full ring-2 ring-offset-4 bg-gray-500 ring-violet-400 ring-offset-gray-800"
+                src={pp}
+              />
+            )}
+            <div className="space-y-2 text-center">
+              <Link to="/userProfile">
+                <h2 className="text-sky-600 text-2xl font-semibold tracking-wide">
+                  {currentUserDetails?.name}
+                </h2>
+              </Link>
               <div>
-                <p className='text-sm text-cyan-900'>Access exclusive tools & insights</p>
+                {currentUserDetails?.headline ? (
+                  <p className="text-yellow-700 text-md">
+                    {currentUserDetails?.headline}
+                  </p>
+                ) : (
+                  <p className="text-red-700 text-md">Add A Headline</p>
+                )}
               </div>
             </div>
+            <hr />
+            <div className="hover:bg-gray-200 rounded-md">
+              <Link to="#">
+                <div>
+                  <div className="flex items-center justify-between text-sm">
+                    <h3 className="text-cyan-900">Connection</h3>
+                    <p className="text-blue-400 font-bold">21</p>
+                  </div>
+                  <h2 className="text-md font-bold text-cyan-900">
+                    Grow Your Network
+                  </h2>
+                </div>
+              </Link>
+            </div>
+            <div>
+              <p className="text-sm text-cyan-900">
+                Access exclusive tools & insights
+              </p>
+            </div>
           </div>
-      }
+        </div>
+      )}
     </div>
   );
 };
