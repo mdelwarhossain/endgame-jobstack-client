efb9f39dfde00ce43731e324c792f4c469a3013d
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2023-02-21 22:53:47 +0600

    category added

--- src/Pages/Jobs/JobCard/JobCard.js
+++ src/Pages/Jobs/JobCard/JobCard.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const JobCard = ({ job }) => {
   // console.log(job)
 
   return (
     <div>
-      <div className='grid  grid-cols-10'>
+      <div className="grid  grid-cols-10">
         <div className=" col-span-3 sm:col-span-3 lg:col-span-2 m-2 mt-4 ml-4">
           <div className="mr-3">
-            <img className="w-24 h-24 rounded" src={job?.logo} alt='' />
+            <img className="w-24 h-24 rounded" src={job?.logo} alt="" />
             {/* <div className="avatar">
               <div >
                 <img src={job?.logo} alt='' />
@@ -17,19 +17,28 @@ const JobCard = ({ job }) => {
             </div> */}
           </div>
         </div>
-        <div className='col-span-7 sm:col-span-7 lg:col-span-6 mt-4'>
+        <div className="col-span-7 sm:col-span-7 lg:col-span-6 mt-4">
           <p className="text-xl font font-bold">{job?.title}</p>
-          <p className='font-semibold'>Coders Solutions Pvt. Ltd. {job?.name}</p>
-          <p className='text-md'>{job?.location}</p>
-          <p className="text-orange-600 text-sm">{job.vacancy} {job?.vacancy > 1 ? 'vacant positions' : 'vacant position'}</p>
+          <p className="font-semibold">
+            Coders Solutions Pvt. Ltd. {job?.name}
+          </p>
+          <p className="text-md">{job?.location}</p>
+          <p className="text-md">{job?.category}</p>
+          <p className="text-orange-600 text-sm">
+            {job.vacancy}{" "}
+            {job?.vacancy > 1 ? "vacant positions" : "vacant position"}
+          </p>
         </div>
-        <div className='col-span-10 lg:col-span-2 mt-4 mx-3 lg:mx-0 lg:mt-10 lg:mr-1'>
-          <Link style={{ backgroundColor: " #00b386" }} to={`/job/${job._id}`}><button className='btn-sm bg-green-700 hover:bg-green-500 text-white font-bold  rounded'>See Details</button></Link>
+        <div className="col-span-10 lg:col-span-2 mt-4 mx-3 lg:mx-0 lg:mt-10 lg:mr-1">
+          <Link style={{ backgroundColor: " #00b386" }} to={`/job/${job._id}`}>
+            <button className="btn-sm bg-green-700 hover:bg-green-500 text-white font-bold  rounded">
+              See Details
+            </button>
+          </Link>
         </div>
       </div>
 
-      <div className='divider'></div>
-
+      <div className="divider"></div>
     </div>
   );
 };
